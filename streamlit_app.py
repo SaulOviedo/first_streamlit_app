02c7445f535ce14b054b14214b996859bab078f4
Saúl Oviedo <sauloviedoleon@gmail.com>
2022-09-05 14:44:11 -0400

    done by now

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas as pd
+
+s3_url = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+my_fruit_list = pd.read_csv(s3_url, index_col='Fruit')
+
 
 streamlit.title('My Moms\'s New Healthy Diner')
 
@@ -8,3 +13,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Smoothie 🍇🍓')
+
+fruits_selected = streamlit.multiselect('Pick some fruits', list(my_fruit_list.index), default=['Banana', 'Apple'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show) 
